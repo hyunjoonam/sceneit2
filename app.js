0b9eb382496db5efc4991bea7613e74d78106259
Hyun Joo Nam <hyunjoooo.nam@gmail.com>
2018-11-28 01:33:18 -0500

    1127 2

--- app.js
+++ app.js
@@ -3,10 +3,12 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const dotenv = require('dotenv');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+dotenv.load();
 var app = express();
 
 // view engine setup
@@ -18,6 +20,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+// setupAuth(app);
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
