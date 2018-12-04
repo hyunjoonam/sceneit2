5e683a5361af404e68baa9ae8b7aa3f14658f474
Hyun Joo Nam <hyunjoooo.nam@gmail.com>
2018-12-04 01:00:22 -0500

    1204

--- models/index.js
+++ models/index.js
@@ -8,9 +8,7 @@ const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
 
-if (process.env.DATABASE_URL) {
-  var sequelize = new Sequelize(process.env.DATABASE_URL);
-} else if (config.use_env_variable) {
+if (config.use_env_variable) {
   var sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
   var sequelize = new Sequelize(config.database, config.username, config.password, config);
